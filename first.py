6ab58cd8d028aa7377a83628e0d383f9079f5808
neramino <ondran125@gmail.com>
2024-10-09 14:15:12 +0000

    sudé a líché čísla

--- first.py
+++ first.py
@@ -4,5 +4,13 @@ def hello():
 #hello()
 
 
+def sudy_nebo_lichy(cislo):
+   
+    if cislo % 2 == 0:
+        print("Číslo",cislo, "je sudé")
+    else:
+        print("Číslo",cislo, "je liché")
 
+sudy_nebo_lichy(5)
 
+sudy_nebo_lichy(1000000)
